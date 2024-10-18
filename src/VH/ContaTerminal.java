5a80d11121814726fe26acfb2b6397321471390c
victor <victorhugosr2005@hotmail.com>
2024-10-18 16:30:17 -0300

    Consertando o main

--- src/VH/ContaTerminal.java
+++ src/VH/ContaTerminal.java
@@ -1,10 +1,40 @@
 package VH;
 
-public class ContaTerminal {
-    int numero;
-    String agencia;
-    String nomeCliente;
-    float saldo;
+import java.util.Scanner;
 
-    
+public class ContaTerminal {
+    public static void main(String[] args) {
+        int numero;
+        String agencia;
+        String nomeCliente;
+        float saldo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o numero da conta bancaria: ");
+        numero = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Digite a agência da conta bancaria: ");
+        agencia = scanner.nextLine();
+
+        System.out.println("Digite seu nome: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o saldo da conta bancaria: ");
+        saldo = Float.parseFloat(scanner.nextLine());
+
+        String texto = "\nOlá ".concat(nomeCliente)
+                .concat(" obrigado por criar uma conta em nosso banco, sua agência é ")
+                .concat(agencia)
+                .concat(", conta ")
+                .concat(String.valueOf(numero))
+                .concat(" e seu saldo ").concat(String.valueOf(saldo))
+                .concat(" já está disponível para saque.");
+
+        System.out.println(texto);
+
+
+
+
+    }
 }
